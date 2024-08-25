f6c260fe7ac10d6c00a9af223fd3f4cd9b41be31
fimaAidarous <fma0899@gmail.com>
2024-08-25 07:21:52 -0700

    Complete Navbar?

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,4 +1,4 @@
-import React, { Children, createContext, useContext, useState } from 'react';
+import React, { children, createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
@@ -11,11 +11,18 @@ const initialState = {
 
 export const ContextProvider = ({ children}) => {
     const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined);
+  
+    const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
     return (
         <StateContext.Provider
         value={{
             activeMenu,
             setActiveMenu,
+            isClicked,setIsClicked,
+            handleClick,
+            screenSize,setScreenSize
         }}
         >
          {children}
